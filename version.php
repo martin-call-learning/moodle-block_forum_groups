055b348a0f9f9db5cd3c0721ee3ff7fed9ae8d3a
Laurent David <lmedavid@gmail.com>
2021-06-28 13:14:35 +0200

    Update block version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_forum_groups';
 $plugin->release = '0.1.0';
-$plugin->version = 2021040101;
+$plugin->version = 2021040102;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
